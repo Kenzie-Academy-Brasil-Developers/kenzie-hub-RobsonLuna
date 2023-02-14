8c11c02ae570e445c3d56db98a51c920a785c4c6
Robson-Luna-BR <robson.luna@outlook.com>
2023-02-14 00:54:06 -0300

    Movida a pasta styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { GlobalStyle } from "./components/styles/global";
+import { GlobalStyle } from "./styles/global";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
